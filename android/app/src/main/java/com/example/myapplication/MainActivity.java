d89f14a6d5367d3d11264621459ad9a495e318c6
k3rnelmaster <k3rnelmaster@gmail.com>
2019-08-16 06:07:55 +0900

    coffee_연습1

--- android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,9 +10,14 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final int QUANTITY_MIN = 0;
+    public static final int QUANTITY_MAX = 10;
+    public static final int COFFEE_PRICE = 3000;
     private Button mMinusButton;
     private Button mPlusButton;
+    private Button mOrderButton;
     private TextView mQuantityTextView;
+    private TextView mResultTextView;
 
     private int mQuantity;
 
@@ -26,16 +30,48 @@ public class MainActivity extends AppCompatActivity {
 
         mMinusButton = findViewById(R.id.minus_button);
         mPlusButton = findViewById(R.id.plus_button);
+        mOrderButton = findViewById(R.id.order_button);
         mQuantityTextView = findViewById(R.id.quantity_text);
+        mResultTextView = findViewById(R.id.result_text);
 
         mMinusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(mQuantity <= QUANTITY_MIN)
+                    ;
+                else
+                    mQuantity = mQuantity - 1;
 
-
+                displayResult();
             }
         });
 
+        mPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mQuantity >= QUANTITY_MAX)
+                    ;
+                else
+                    mQuantity = mQuantity + 1;
+
+                displayResult();
+            }
+        });
+
+        mOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = mResultTextView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void displayResult() {
+        mQuantityTextView.setText("" + mQuantity);
+        String result = "가격: " + (COFFEE_PRICE * mQuantity) + "원\n감사합니다.";
+        mResultTextView.setText(result);
     }
 
     private void init() {
